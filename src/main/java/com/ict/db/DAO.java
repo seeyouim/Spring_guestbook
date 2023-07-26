94803c30992ccffd18eee35c767307f7e1299973
JSY <wpdnjsdl1998@gmail.com>
2023-07-26 18:20:06 +0900

    20230726

---
+++ src/main/java/com/ict/db/DAO.java
@@ -0,0 +1,24 @@
+package com.ict.db;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+public class DAO {
+	private  SqlSessionTemplate sqlSessionTemplate;
+
+	public SqlSessionTemplate getSqlSessionTemplate() {
+		return sqlSessionTemplate;
+	}
+
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+		this.sqlSessionTemplate = sqlSessionTemplate;
+	}
+	public List<VO> getList() {
+		List<VO> list = sqlSessionTemplate.selectList("guestbook.list");
+		return list;
+	}
+	
+}
